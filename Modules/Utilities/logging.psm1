a52557dae9635242d6869417155072ca1b072f89
lukianp <lukianp@hotmail.com>
2025-05-29 19:41:56 +0100

    Update logging.psm1

--- Modules/Utilities/logging.psm1
+++ Modules/Utilities/logging.psm1
@@ -108,7 +108,7 @@ function Write-MandALog {
     if ($script:LoggingConfig.FileOutput -and $script:LoggingConfig.LogFile) {
         try {
             # Clean message for file output (remove emojis if needed)
-            $fileMessage = $Message -replace '[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]', ''
+           $fileMessage = $Message -replace '[\uD83C-\uDBFF\uDC00-\uDFFF]+', ''
             $fileLogEntry = "[$timestamp] [$Level] [$Component] $fileMessage"
             
             Add-Content -Path $script:LoggingConfig.LogFile -Value $fileLogEntry -Encoding UTF8
