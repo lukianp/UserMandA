75cb2592ee21e08a7532db7ad45a43306de6d7c0
lukianp <lukianp@hotmail.com>
2025-06-04 16:25:10 +0100

    Update IntuneDiscovery.psm1

--- Modules/Discovery/IntuneDiscovery.psm1
+++ Modules/Discovery/IntuneDiscovery.psm1
@@ -16,16 +16,6 @@
 
 
 
-#Updated global logging thingy
-        if ($null -eq $global:MandA) {
-    throw "Global environment not initialized"
-}
-        $outputPath = $Context.Paths.RawDataOutput
-
-        if (-not (Test-Path $Context.Paths.RawDataOutput)) {
-    New-Item -Path $Context.Paths.RawDataOutput -ItemType Directory -Force
-}
-
 
 # --- Helper Functions (Assumed to be available globally) ---
 # Export-DataToCSV
