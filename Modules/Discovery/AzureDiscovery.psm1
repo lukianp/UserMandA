bb356cee3908d42ff79f1a9155c35e73a058919d
lukianp <lukianp@hotmail.com>
2025-06-04 16:27:11 +0100

    Update AzureDiscovery.psm1

--- Modules/Discovery/AzureDiscovery.psm1
+++ Modules/Discovery/AzureDiscovery.psm1
@@ -558,15 +558,6 @@ function Export-AzureDiscoveryData {
     
    
 
-#Updated global logging thingy
-        if ($null -eq $global:MandA) {
-    throw "Global environment not initialized"
-}
-        $outputPath = $Context.Paths.RawDataOutput
-
-        if (-not (Test-Path $Context.Paths.RawDataOutput)) {
-    New-Item -Path $Context.Paths.RawDataOutput -ItemType Directory -Force
-}
     
     foreach ($key in $DiscoveredData.Keys) {
         $dataList = $DiscoveredData[$key]
