f6f481d170769fabaa63aaf645448e393274ae73
lukianp <lukianp@hotmail.com>
2025-08-05 15:19:37 +0100

    cleanup

--- test-credentials.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module 'C:\enterprisediscovery\net6.0-windows\Modules\Core\CredentialLoader.psm1' -Force
-$credentials = Get-CompanyCredentials -CompanyName 'ljpops'
-Write-Host "ExpiryDate: $($credentials.ExpiryDate)"
-$expiryCheck = Test-CredentialExpiry -Credentials $credentials
-Write-Host "Valid: $($expiryCheck.Valid)"
-Write-Host "Message: $($expiryCheck.Message)"
-if ($expiryCheck.Warning) { 
-    Write-Host "Warning: $($expiryCheck.Warning)" 
-}
